29c522a1f37dde2030427b61e4db907a43b991dc
Rahul Sharma <rahul.sharma1@swiggy.in>
2020-05-01 21:54:46 +0530

    removed email contraints

--- contacts/models/contact.py
+++ contacts/models/contact.py
@@ -5,5 +5,5 @@ from django.contrib.auth.models import User
 class Contact(AbstractBaseModel):
     user = models.ForeignKey(User, on_delete=models.PROTECT)
     name = models.CharField(max_length=255)
-    email = models.EmailField(null=True, blank=False, unique=True)
+    email = models.EmailField(blank=False, unique=True)
     phone_number = models.CharField(max_length=10)
